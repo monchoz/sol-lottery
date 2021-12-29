bde95436bfbe164f92665105f55d0f38d4d07a0b
Ramon Zuniga <ramon.zuniga@freshbooks.com>
2021-12-29 15:29:05 -0600

    updated to solidity 0.8.11

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,28 +1,28 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.8.11;
 
 contract Lottery {
     address public manager;
-    address[] public players;
+    address payable[] public players;
     address public lastWinner;
     
-    function Lottery() public {
+    constructor() {
         manager = msg.sender;
     }
     
     function enter() public payable {
         require(msg.value > .01 ether);
-        players.push(msg.sender);
+        players.push(payable(msg.sender));
     }
     
     function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
     }
     
     function pickWinner() public restricted {
         uint index = random() % players.length;
-        players[index].transfer(this.balance);
+        players[index].transfer(address(this).balance);
         lastWinner = players[index];
-        players = new address[](0);
+        players = new address payable[](0);
     }
     
     modifier restricted() {
@@ -30,7 +30,7 @@ contract Lottery {
         _;
     }
     
-    function getPlayers() public view returns (address[]) {
+    function getPlayers() public view returns (address payable[] memory) {
         return players;
     }
 }   
